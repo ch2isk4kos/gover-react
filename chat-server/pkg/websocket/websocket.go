2bed98319340f8674d39bd604eb628f3150ce7a0
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-08 01:18:50 -0500

    revise websocket package

--- chat-server/pkg/websocket/websocket.go
+++ chat-server/pkg/websocket/websocket.go
@@ -1,8 +1,6 @@
 package websocket
 
 import (
-	"fmt"
-	"io"
 	"log"
 	"net/http"
 
@@ -18,55 +16,10 @@ var upgrader = websocket.Upgrader{
 }
 
 func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
-	ws, err := upgrader.Upgrade(w, r, nil)
+	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
-		return ws, err
+		return nil, err
 	}
-	return ws, nil
-}
-
-// define reader to listen for new messages sent to ws endpoint
-func Reader(conn *websocket.Conn) {
-	for {
-		// read message
-		message, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		// console message
-		fmt.Println(string(p))
-
-		// write message
-		if err := conn.WriteMessage(message, p); err != nil {
-			log.Println(err)
-			return
-		}
-	}
-}
-
-func Writer(conn *websocket.Conn) {
-	for {
-		fmt.Println("Sending...")
-		message, r, err := conn.NextReader()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		w, err := conn.NextWriter(message)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		if _, err := io.Copy(w, r); err != nil {
-			fmt.Println(err)
-			return
-		}
-		if err := w.Close(); err != nil {
-			fmt.Println(err)
-			return
-		}
-	}
+	return conn, nil
 }

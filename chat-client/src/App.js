412266b35b7bcd6c67f5961722135a6af2810d2d
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-07 22:37:08 -0500

    add and passed state to child component

--- chat-client/src/App.js
+++ chat-client/src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import Header from './Header';
+import Messages from './Messages';
 import { connect, sendMessage } from './api/index.js';
 import './App.css';
 class App extends React.Component {
   constructor(props) {
     super(props)
-    
     connect();
-  }
+
+    this.state = {
+      messages: []
+    }
+  };
+
+  componentDidMount() {
+    connect((msg) => {
+      console.log("New Message...");
+      this.setState(prevState => ({
+        messages: [...this.state.messages, msg]
+      }), console.log(this.state))
+    });
+  };
 
   handleSubmission() {
     console.log("Sending Message from App.js");
-    sendMessage("This the message that I sent.")
+    sendMessage("This is the message that I sent.")
   }
 
   render() {
@@ -19,6 +32,7 @@ class App extends React.Component {
       <div className="App">
         <Header />
         <button onClick={this.handleSubmission}>Send</button>
+        <Messages messages={this.state.messages} />
       </div>
     );
   }

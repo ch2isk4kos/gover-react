0e62bbd354732ae3ad290fb9e3a942146c309798
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-07 21:22:34 -0500

    create directory for api and add file that exports listeners for connection and sending messages to WebSocket endpoint

---
+++ chat-client/api/index.js
@@ -0,0 +1,30 @@
+const socket = new WebSocket("ws://localhost:3000/chatroom");
+
+// connect to WebSocket endpoint and listen for events
+let connect = () => {
+  console.log("Connecting...");
+
+  socket.onopen = () => {
+    console.log("Connection Successful.");
+  };
+
+  socket.onmessage = msg => {
+    console.log(msg);
+  };
+
+  socket.onclose = event => {
+    console.log("Lost Connection: ", event);
+  };
+
+  socket.onerror = err => {
+    console.log("WebSocket Error: ", err);
+  };
+};
+
+// send messages to react client
+let sendMessage = msg => {
+  console.log("Sending Message: ", msg);
+  socket.send(msg);
+}
+
+export { connect, sendMessaage };

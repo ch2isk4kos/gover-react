3fa9bbc2e2b6193f334d90b7cf0f4d09a0ec8c42
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-08 00:05:29 -0500

    add funcionality from import

--- chat-server/main.go
+++ chat-server/main.go
@@ -2,21 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/ch2isk4kos/gover-react/pkg/websocket"
 )
 
 var PORT string = ":3000"
-
-// create uprader to read/write buffer size
-var upgrader = websocket.Upgrader{
-	ReadBufferSize: 1024,
-	WriteBufferSize: 1024,
-	// check origin of connection to accept request from React
-	CheckOrigin: func(r *http.Request) bool { return true },
-}
 
 func main() {
 	initAndConfigRoutes()
@@ -25,25 +16,19 @@ func main() {
 }
 
 func initAndConfigRoutes() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, `Already Home`)
-	})
 	http.HandleFunc("/chatroom", serveWs)
 }
 
 // define WebSocket endpoint
 func serveWs(w http.ResponseWriter, r *http.Request) {
-	// console host address
-	fmt.Println(r.Host)
-
 	// upgrade to websocket connection
-	ws, err := upgrader.Upgrade(w, r, nil)
+	ws, err := websocket.Upgrade(w, r)
 	if err != nil {
-		log.Println(err)
+		fmt.Println(err)
 	}
 
-	// openly listen for incoming messages
-	reader(ws)
+	go websocket.Writer(ws)
+	websocket.Reader(ws)
 }
 
 

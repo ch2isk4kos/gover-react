3a8f59f4cd75ba8c5d477e201b0cdb9c5810cd41
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-09 23:08:24 -0500

    import and render message component

--- chat-client/src/ChatRoom.js
+++ chat-client/src/ChatRoom.js
@@ -1,19 +1,17 @@
 import React from 'react';
+import Message from './Message';
 import './ChatRoom.css';
 
 class ChatRoom extends React.Component {
   render() {
-    const msgs = this.props.messages.map((msg, index) => {
-      console.log("messages[msg]:", msg)
-      return <p key={index}>{ msg.data }</p>
-    })
+    const chat = this.props.messages.map(msg => <Message key={msg.timeStamp} message={msg.data} /> );
 
     console.log("Messages props: ", this.props);
     
     return (
       <div className="Messages">
         <h2>Chatroom</h2>
-        { msgs }
+        <span crossOrigin="true">{ chat }</span>
       </div>
     )
   }
